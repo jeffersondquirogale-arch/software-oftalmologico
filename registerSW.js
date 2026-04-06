b471a08f0e478cbdd95f1862f6977b81c9d0fc5c
jeffersondquirogale-arch <jeffersond.quirogale@ecci.edu.co>
2026-04-06 00:10:49 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/software-oftalmologico/sw.js', { scope: '/software-oftalmologico/' })})}
